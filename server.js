9a914ff574a3e8bdc45ff264a2a845eb50c1ff40
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 16:34:03 -0400

    prueba cors 4

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var app = express()
 
 app.use(cors())
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")<
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "dist/mf-portal")))
   .get("*", (req, res) => {
     res.sendFile("index.html", { root: "dist/mf-portal" });
