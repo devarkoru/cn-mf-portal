229107c4d8e8a9fbeb0571551439fcf523f7e564
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 18:24:41 -0400

    prueba cors api

--- server.js
+++ server.js
@@ -2,30 +2,16 @@ const express = require("express");
 const cors = require('cors');
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-const morgan = require("morgan");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const API_SERVICE_URL = "https://la2.api.riotgames.com";
 
 var app = express()
 
 app.use(cors())
 
-app.use('', (req, res, next) => {
-   if (req.headers.authorization) {
-       next();
-   } else {
-       res.sendStatus(403);
-   }
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")<
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
-
-// Proxy endpoints
-app.use('/lol/league/v4/entries/by-summoner/', createProxyMiddleware({
-  target: API_SERVICE_URL,
-  changeOrigin: true,
-  pathRewrite: {
-      [`^/lol/league/v4/entries/by-summoner/`]: '',
-  },
-}));
 
 app.use(express.static(path.join(__dirname, "dist/mf-portal")))
   .get("*", (req, res) => {
