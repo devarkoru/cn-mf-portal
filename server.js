bf0e22d6dfeba951a815e6e3d7bf6cff61367176
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-24 01:29:50 -0400

    prueba cors api

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'X-RIOT-TOKEN, Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 
