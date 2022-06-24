385a64f396f7e9b7dbe4912f1d36a926b48a2c5a
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-24 14:04:06 -0400

    prueba cors con api riot

--- server.js
+++ server.js
@@ -9,19 +9,8 @@ app.use(cors())
 
 app.options('*', cors());
 
-var whitelist = ['https://la2.api.riotgames.com']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('X-RIOT-TOKEN')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-
 app.use(express.static(path.join(__dirname, "dist/mf-portal")))
-  .get("*", cors(corsOptionsDelegate), (req, res) => {
+  .get("*", (req, res) => {
     res.sendFile("index.html", { root: "dist/mf-portal" });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
