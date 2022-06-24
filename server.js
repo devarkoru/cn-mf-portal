d509d9140638717f9c66602d185d35ca6ce57391
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-24 01:25:59 -0400

    prueba cors api

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ var app = express()
 
 app.use(cors())
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin: *');
-  res.header("Access-Control-Allow-Headers: X-RIOT-TOKEN, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-  res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-})
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'X-RIOT-TOKEN, Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.use(express.static(path.join(__dirname, "dist/mf-portal")))
   .get("*", (req, res) => {
