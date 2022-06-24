47df35c2afda45052729d226ee75e8ba29ab4665
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 22:27:46 -0400

    prueba cors api

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ var app = express()
 
 app.use(cors())
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static(path.join(__dirname, "dist/mf-portal")))
   .get("*", (req, res) => {
     res.sendFile("index.html", { root: "dist/mf-portal" });
