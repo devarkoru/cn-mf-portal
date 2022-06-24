b2b91120208096d15acd1f88990ae7ad813e0dca
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 22:16:11 -0400

    prueba cors api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   RIOT_API_DEV_KEY: 'RGAPI-eae978ab-e133-4eb7-bac0-341eab2b25b8',
-  summonerEncript: 'XaS9cRa4uvbCtf3Uq0AurA4aKbgixq9EYD4hks2Zzp4j'
+  summonerEncript: 'XaS9cRa4uvbCtf3Uq0AurA4aKbgixq9EYD4hks2Zzp4j',
+  RIOT_API_URL: 'https://la2.api.riotgames.com'
 };
