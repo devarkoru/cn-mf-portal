50ad359fa8ff1a0c036f552a1f4e620789963e20
Claudio Núñez Oyarzún <arkrater@gmail.com>
2022-06-23 22:54:21 -0400

    prueba cors api

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ app.use(cors())
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
+    "Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   if (req.method === "OPTIONS") {
     res.header(
       "Access-Control-Allow-Methods",
